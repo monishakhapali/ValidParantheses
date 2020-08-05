d8454041eaae685e558d99eb1e36bbc6db152b28
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-08-05 17:00:57 -0400

    validate parantheses

---
+++ ValidParantheses/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace ValidParantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string s = "[()(){}]";
+            int i = 0;
+            Stack stack = new Stack();
+            bool flag = false;
+            if (s.Length % 2 != 0)
+                flag = false;
+            foreach (var c in s)
+            {
+                if (c == '(' || c == '[' || c=='{')
+                {
+                    stack.Push(c);
+                }
+                if(c ==')' && stack.Count!=0 && stack.Peek().ToString() =="(" )
+                {
+                    stack.Pop();
+                }
+                if (c == ']' && stack.Count != 0 && stack.Peek().ToString() == "[")
+                {
+                    stack.Pop();
+                }
+                if (c == '}' && stack.Count != 0 && stack.Peek().ToString() == "{")
+                {
+                    stack.Pop();
+                }
+            }
+            if(stack.Count != 0)
+            {
+                flag = false;
+            }
+            else
+            {
+                flag = true;
+            }
+            Console.WriteLine(flag);
+            Console.ReadLine();
+        }
+    }
+}
